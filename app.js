003309f20b12192b6161c123502fc7c4102d153b
Mohammed Rakib <mohammedrakib81336@gmail.com>
2021-02-07 23:19:20 +0600

    second commit

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ searchFood.addEventListener("click", () => {
     .then(response => response.json())
     .then(data => displayFoods(data))
 
-
 })
 
 displayFoods = foods =>{
     const row1 = document.getElementById("row1");
+    const singleItem = document.getElementById("itemShow");
     row1.innerText='';
         const foodArr = foods.meals;
         for (let i = 0; i < foodArr.length; i++) {
             const element = foodArr[i];
+            console.log(element);
             const col1 = document.createElement("div");
             col1.className= "col-lg-4";
         const coloum1 = `
@@ -27,6 +28,26 @@ displayFoods = foods =>{
         col1.innerHTML=coloum1;
 
         row1.appendChild(col1);
+
+        col1.addEventListener("click",function(){
+            const col1 = document.createElement("div");
+            singleItem.innerText='';
+        const coloum1 = `
+           <img src="${element.strMealThumb}">
+           <h2>${element.strMeal}<h2>
+           <h2>Ingredients</h2>
+           <p>${element.strIngredient1}</p>
+           <p>${element.strIngredient2}</p>
+           <p>${element.strIngredient3}</p>
+           <p>${element.strIngredient4}</p>
+           <p>${element.strIngredient5}</p>
+           <p>${element.strIngredient6}</p>
+        `
+        col1.innerHTML=coloum1;
+
+        singleItem.appendChild(col1);
+
+        })
 
         }
 }
